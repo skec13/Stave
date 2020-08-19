5f31153c07487a4ed663e67c1a8c5d81bc9128cc
unknown <Babnik21>
2020-08-20 01:44:47 +0200

    Now loops through matches, saves data in list

--- Stave.py
+++ Stave.py
@@ -3,6 +3,8 @@ import bs4
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
+import requests
+import time
 
 value = "Barcelona"
 
@@ -15,7 +17,7 @@ search_button = browser.find_element_by_css_selector(".header__buttonIcon--searc
 search_button.click()
 search_bar = browser.find_element_by_css_selector("#search-form-query")
 search_bar.send_keys(value)
-browser.implicitly_wait(1)
+browser.implicitly_wait(2)
 result_element = browser.find_element_by_css_selector("#search-results > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1) > a:nth-child(1)")
 result_element.click()
 
@@ -24,22 +26,23 @@ games = browser.find_elements_by_class_name("event__match")
 game_links = []
 for game in games:
     game_id = game.get_attribute("id")
-    game_links.append("https://www.flashscore.com/match/" + game_id + "/#match-summary")
+    game_links.append("https://www.flashscore.com/match/" + game_id[4:] + "/#match-statistics;0")
 
-#Visits every game stats site
+#Visits every game stats site and scraps data into lists
+huge_list = []
 for page in game_links:
     browser.get(page)
-    stats_element = browser.find_element_by_css_selector("#a-match-statistics")
-    stats_element.click()
-    #Missing the scraping part
-    lineups_element = browser.find_element_by_css_selector("#a-match-lineups")
-    lineups_element.click()
-    #Missing the scraping part
-    odds_element = browser.find_element_by_css_selector("#a-match-odds")
-    odds_element.click()
-    #Missing the scraping part
+    element = browser.find_elements_by_class_name("statTextGroup")  #Unnecessary?
+    soup = bs4.BeautifulSoup(browser.page_source, "html.parser")
+    tag = soup.find_all('div', {"class": "statTextGroup"})
+    medium_list = []
+    for el in tag:
+        stat = []
+        mini_list = el.find_all('div')
+        for el in mini_list:
+            stat.append(el.text)
+        medium_list.append(stat)
+    huge_list.append(medium_list)
 
-
-
-
+browser.quit()
 
