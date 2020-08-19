c530edd53f41b6c7c5202cfae2c28bf09b443ba9
unknown <Babnik21>
2020-08-19 17:50:21 +0200

    Vpiše izbrano ekipo v search

--- Stave.py
+++ Stave.py
@@ -1,4 +1,12 @@
 from selenium import webdriver
 import bs4
 
+value = input("Izberi ekipo: ")
+
+
 browser = webdriver.Firefox()
+browser.get("https://www.flashscore.com")
+elem = browser.find_element_by_css_selector(".header__buttonIcon--search")
+elem.click()
+searchElem = browser.find_element_by_css_selector("#search-form-query")
+searchElem.send_keys(value)
