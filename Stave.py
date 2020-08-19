7c6dfba6f525dcaf3c8ef4768f74eed7d01d994a
unknown <Babnik21>
2020-08-19 20:07:12 +0200

    Now visits every rececnt played game site

--- Stave.py
+++ Stave.py
@@ -19,11 +19,25 @@ browser.implicitly_wait(1)
 result_element = browser.find_element_by_css_selector("#search-results > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1) > a:nth-child(1)")
 result_element.click()
 
-#Extracts game id's into a list
+#Extracts game id's into a list of links
 games = browser.find_elements_by_class_name("event__match")
-game_ids = []
+game_links = []
 for game in games:
-    game_ids.append(game.get_attribute("id"))
+    game_id = game.get_attribute("id")
+    game_links.append("https://www.flashscore.com/match/" + game_id + "/#match-summary")
+
+#Visits every game stats site
+for page in game_links:
+    browser.get(page)
+    stats_element = browser.find_element_by_css_selector("#a-match-statistics")
+    stats_element.click()
+    #Missing the scraping part
+    lineups_element = browser.find_element_by_css_selector("#a-match-lineups")
+    lineups_element.click()
+    #Missing the scraping part
+    odds_element = browser.find_element_by_css_selector("#a-match-odds")
+    odds_element.click()
+    #Missing the scraping part
 
 
 
