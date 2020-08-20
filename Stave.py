d4542b65190c734b14e098be0a15ab4b4c73bb5c
matevz <matevz.miskec@gmail.com>
2020-08-20 14:28:42 +0200

    ok

--- Stave.py
+++ Stave.py
@@ -1,5 +1,4 @@
-print(111)
-print(222)
+
 from selenium import webdriver
 import bs4
 from selenium.webdriver.common.keys import Keys
@@ -22,6 +21,13 @@ search_bar.send_keys(value)
 browser.implicitly_wait(2)
 result_element = browser.find_element_by_css_selector("#search-results > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1) > a:nth-child(1)")
 result_element.click()
+
+#Shows more matches
+show_matches = browser.find_element_by_css_selector(".event__more")
+show_matches.click()
+browser.implicitly_wait(2) #wait for 2 secords(maybe 1 second is enough??)
+show_matches1 = browser.find_element_by_css_selector(".event__more")
+show_matches1.click()
 
 #Extracts game id's into a list of links
 games = browser.find_elements_by_class_name("event__match")
